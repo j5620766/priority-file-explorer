22bd61630dec3bc54f766ecc703d7c1c33b1a7c7
전현규 <yytss3@gmail.com>
2025-04-14 21:35:37 +0900

    파일 추가 및 실행
    drag and drop으로 파일 추가

---
+++ priority-file-explorer-/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace priority_file_explorer_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.추가ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1488, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.추가ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 추가ToolStripMenuItem
+            // 
+            this.추가ToolStripMenuItem.Name = "추가ToolStripMenuItem";
+            this.추가ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.추가ToolStripMenuItem.Text = "추가";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 36);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1464, 826);
+            this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.FlowLayoutPanel1_DragDrop);
+            this.flowLayoutPanel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.FlowLayoutPanel1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1488, 874);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 추가ToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ priority-file-explorer-/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace priority_file_explorer_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form_DragEnter);
+            this.DragDrop += new DragEventHandler(Form_DragDrop);
+
+            
+
+            InitializeComponent();
+        }
+
+        private void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string filePath in files)
+            {
+                
+            }
+        }
+
+        private void FlowLayoutPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void FlowLayoutPanel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                // 1. 썸네일용 PictureBox
+                PictureBox pb = new PictureBox();
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Size = new Size(64, 64);
+                pb.Margin = new Padding(0, 0, 0, 5);
+                pb.Tag = file;
+
+                string ext = Path.GetExtension(file).ToLower();
+
+                if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".bmp" || ext == ".gif")
+                {
+                    try
+                    {
+                        Image img = Image.FromFile(file);
+                        pb.Image = new Bitmap(img, pb.Size);
+                    }
+                    catch
+                    {
+                        pb.Image = SystemIcons.Warning.ToBitmap();
+                    }
+                }
+                else
+                {
+                    pb.Image = Icon.ExtractAssociatedIcon(file).ToBitmap();
+                }
+
+                // 2. 파일 이름용 Label
+                Label lbl = new Label();
+                lbl.Text = Path.GetFileName(file);
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                lbl.AutoSize = false;
+                lbl.Width = 64;
+                lbl.Height = 30;
+                lbl.MaximumSize = new Size(64, 40);
+                lbl.Font = new Font("맑은 고딕", 8);
+                lbl.ForeColor = Color.Black;
+
+                // 3. PictureBox + Label을 담을 Panel 생성
+                Panel filePanel = new Panel();
+                filePanel.Width = 70;
+                filePanel.Height = 100;
+                filePanel.Margin = new Padding(5);
+                filePanel.Controls.Add(pb);
+                filePanel.Controls.Add(lbl);
+
+                // 위치 조정
+                pb.Location = new Point(3, 0);
+                lbl.Location = new Point(0, 68);
+
+                EventHandler MouseDoubleClick = (s, ev) =>
+                {
+                    try
+                    {
+                        var psi = new ProcessStartInfo(file)
+                        {
+                            UseShellExecute = true
+                        };
+                        Process.Start(psi);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("파일 실행 실패: " + ex.Message);
+                    }
+                };
+
+                // 클릭 시 배경색 변경
+                EventHandler clickHandler = (s, ev) =>
+                {
+                    try
+                    {
+                        // 클릭 시 배경색 변경 (파란색)
+                        if (filePanel.BackColor == Color.LightBlue)
+                        {
+                            filePanel.BackColor = Color.Transparent;  // 원래 색으로 돌아감
+                        }
+                        else
+                        {
+                            filePanel.BackColor = Color.LightBlue;  // 파란색으로 변경
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("파일 실행 실패: " + ex.Message);
+                    }
+                };
+
+                // AddClickHandler 부분 제거 (이미 클릭 이벤트는 등록되어 있음)
+                flowLayoutPanel1.Controls.Add(filePanel);
+
+                AddDoubleClickHandler(filePanel, MouseDoubleClick);
+                AddClickHandler(filePanel, clickHandler);
+                
+
+            }
+        }
+
+        void AddClickHandler(Control parent, EventHandler handler)
+        {
+            parent.Click += handler;  // 부모 컨트롤에도 클릭 이벤트 추가
+
+            foreach (Control child in parent.Controls)
+            {
+                child.Click += handler;  // 자식 컨트롤에도 클릭 이벤트 추가
+            }
+        }
+        void AddDoubleClickHandler(Control parent, EventHandler handler)
+        {
+            parent.DoubleClick += handler;  // 부모 컨트롤에도 클릭 이벤트 추가
+
+            foreach (Control child in parent.Controls)
+            {
+                child.DoubleClick += handler;  // 자식 컨트롤에도 클릭 이벤트 추가
+            }
+        }
+    }
+}
