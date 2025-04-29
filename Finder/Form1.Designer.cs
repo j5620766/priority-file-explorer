17690835217b72c45bf2e47b149f07db816dc55c
Han Seung Jun <97959859+bori99@users.noreply.github.com>
2025-04-29 20:19:38 +0900

    search & find
    ver_01

---
+++ Finder/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lvwFiles = new System.Windows.Forms.ListView();
+            this.colFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFileSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFileDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cmuListView = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDetail = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuList = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSmall = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuLarge = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.trvDir = new System.Windows.Forms.TreeView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tsmiRename = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.cmuListView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 57);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(100, 19);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(339, 25);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "전체경로";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lvwFiles);
+            this.panel2.Controls.Add(this.splitter1);
+            this.panel2.Controls.Add(this.trvDir);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 393);
+            this.panel2.TabIndex = 1;
+            // 
+            // lvwFiles
+            // 
+            this.lvwFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colFileName,
+            this.colFileSize,
+            this.colFileDate});
+            this.lvwFiles.ContextMenuStrip = this.cmuListView;
+            this.lvwFiles.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lvwFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwFiles.HideSelection = false;
+            this.lvwFiles.LargeImageList = this.imgList;
+            this.lvwFiles.Location = new System.Drawing.Point(168, 0);
+            this.lvwFiles.Name = "lvwFiles";
+            this.lvwFiles.Size = new System.Drawing.Size(632, 393);
+            this.lvwFiles.SmallImageList = this.imgList;
+            this.lvwFiles.TabIndex = 2;
+            this.lvwFiles.UseCompatibleStateImageBehavior = false;
+            this.lvwFiles.SelectedIndexChanged += new System.EventHandler(this.lvwFiles_SelectedIndexChanged);
+            this.lvwFiles.DoubleClick += new System.EventHandler(this.lvwFiles_DoubleClick);
+            // 
+            // cmuListView
+            // 
+            this.cmuListView.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuDetail,
+            this.mnuList,
+            this.mnuSmall,
+            this.mnuLarge,
+            this.tsmiRename,
+            this.tsmiDelete});
+            this.cmuListView.Name = "cmuListView";
+            this.cmuListView.Size = new System.Drawing.Size(211, 200);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(210, 24);
+            this.mnuOpen.Text = "열기";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuDetail
+            // 
+            this.mnuDetail.Name = "mnuDetail";
+            this.mnuDetail.Size = new System.Drawing.Size(210, 24);
+            this.mnuDetail.Text = "자세히";
+            this.mnuDetail.Click += new System.EventHandler(this.mnuDetail_Click);
+            // 
+            // mnuList
+            // 
+            this.mnuList.Name = "mnuList";
+            this.mnuList.Size = new System.Drawing.Size(210, 24);
+            this.mnuList.Text = "간단히";
+            this.mnuList.Click += new System.EventHandler(this.mnuList_Click);
+            // 
+            // mnuSmall
+            // 
+            this.mnuSmall.Name = "mnuSmall";
+            this.mnuSmall.Size = new System.Drawing.Size(210, 24);
+            this.mnuSmall.Text = "작은 아이콘";
+            this.mnuSmall.Click += new System.EventHandler(this.mnuSmall_Click);
+            // 
+            // mnuLarge
+            // 
+            this.mnuLarge.Name = "mnuLarge";
+            this.mnuLarge.Size = new System.Drawing.Size(210, 24);
+            this.mnuLarge.Text = "큰 아이콘";
+            this.mnuLarge.Click += new System.EventHandler(this.mnuLarge_Click);
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "a.png");
+            this.imgList.Images.SetKeyName(1, "b.png");
+            this.imgList.Images.SetKeyName(2, "c.png");
+            this.imgList.Images.SetKeyName(3, "d.png");
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(165, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 393);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // trvDir
+            // 
+            this.trvDir.Dock = System.Windows.Forms.DockStyle.Left;
+            this.trvDir.ImageIndex = 0;
+            this.trvDir.ImageList = this.imgList;
+            this.trvDir.Location = new System.Drawing.Point(0, 0);
+            this.trvDir.Name = "trvDir";
+            this.trvDir.SelectedImageIndex = 0;
+            this.trvDir.Size = new System.Drawing.Size(165, 393);
+            this.trvDir.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(472, 18);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(216, 25);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(719, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+           
+            // 
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.cmuListView.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView lvwFiles;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TreeView trvDir;
+        private System.Windows.Forms.ColumnHeader colFileName;
+        private System.Windows.Forms.ColumnHeader colFileSize;
+        private System.Windows.Forms.ColumnHeader colFileDate;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.ContextMenuStrip cmuListView;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuDetail;
+        private System.Windows.Forms.ToolStripMenuItem mnuList;
+        private System.Windows.Forms.ToolStripMenuItem mnuSmall;
+        private System.Windows.Forms.ToolStripMenuItem mnuLarge;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRename;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDelete;
+    }
+}
+
---
+++ Finder/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Finder
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.trvDir.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.trvDir_BeforeExpand);
+            this.trvDir.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.trvDir_BeforeSelect);
+            
+        }
+
+        // 2) Form1_Load: 폼 로드 시 논리 드라이브(예: C:\, D:\)를 트리뷰에 추가
+        private void Form1_Load(object sender, EventArgs e)
+        {// 시스템의 모든 논리 드라이브 문자열 배열
+            string[] Drv_list;
+            TreeNode root;
+
+            Drv_list = Environment.GetLogicalDrives();
+
+            foreach (string Drv in Drv_list)
+            {// 트리뷰 최상위에 드라이브 노드 추가
+                root = trvDir.Nodes.Add(Drv); 
+                root.ImageIndex = 2;
+
+                if (trvDir.SelectedNode == null)
+                    trvDir.SelectedNode = null;
+                root.SelectedImageIndex = root.ImageIndex;
+                // 자식 노드를 미리 하나 추가해 두어 “+” 표시가 나타나도록 함
+                root.Nodes.Add("");
+            }
+        }
+
+        // 3) setPlus: 특정 노드에 하위 폴더가 있으면 “+” 더미 노드 추가
+        public void setPlus(TreeNode node)
+        {
+            string path;
+            DirectoryInfo dir;
+            DirectoryInfo[] di;
+
+            try
+            {
+                path = node.FullPath; // 노드의 전체 경로
+                dir = new DirectoryInfo(path);
+                di = dir.GetDirectories(); // 하위 디렉터리 목록
+                if (di.Length > 0) // 하위 디렉터리가 하나라도 있으면 더미로 빈 노드 추가
+                    node.Nodes.Add("");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void OpenFiles()
+        {
+            ListView.SelectedListViewItemCollection siList;
+            siList = lvwFiles.SelectedItems;
+
+            foreach (ListViewItem item in siList)
+            {
+                OpenItem(item);
+            }
+        }
+
+        
+        /// trvDir.Nodes 전체를 재귀 탐색해서 FullPath가 path와 일치하는 TreeNode를 반환      
+        private TreeNode FindNodeByPath(TreeNodeCollection nodes, string path)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                // 트리뷰 노드의 FullPath 속성과 비교
+                if (string.Equals(node.FullPath, path, StringComparison.InvariantCultureIgnoreCase))
+                    return node;
+
+                // 자식이 “+”만 달려 있는 더미 노드 상태라면 실제 하위 폴더 채우기
+                if (node.Nodes.Count == 1 && node.Nodes[0].Text == "")
+                    setPlus(node);
+
+                // 재귀 탐색
+                TreeNode found = FindNodeByPath(node.Nodes, path);
+                if (found != null)
+                    return found;
+            }
+            return null;
+        }
+
+        public void OpenItem(ListViewItem item)
+        {
+            // Tag에 저장해 둔 풀 경로 꺼내기
+            string fullPath = item.Tag as string;
+            if (Directory.Exists(fullPath))
+            {
+                // 트리뷰에서 노드 찾아 선택·확장
+                TreeNode target = FindNodeByPath(trvDir.Nodes, fullPath);
+                if (target != null)
+                {
+                    trvDir.SelectedNode = target;
+                    target.Expand();
+                    trvDir.Focus();
+                }
+                else
+                {
+                    // 없으면 부모 노드 찾아서 새로 추가
+                    string parent = Path.GetDirectoryName(fullPath);
+                    TreeNode pnode = FindNodeByPath(trvDir.Nodes, parent);
+                    if (pnode != null)
+                    {
+                        TreeNode node = pnode.Nodes.Add(Path.GetFileName(fullPath));
+                        setPlus(node);
+                        trvDir.SelectedNode = node;
+                        node.Expand();
+                        trvDir.Focus();
+                    }
+                }
+                // txtPath도 실제 선택된 폴더로 갱신
+                txtPath.Text = fullPath;
+            }
+            else if (File.Exists(fullPath))
+            {
+                // 파일은 바로 실행
+                Process.Start(fullPath);
+            }
+        }
+
+
+
+        private void trvDir_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            string path;
+            DirectoryInfo dir;
+            DirectoryInfo[] di;
+            TreeNode node;
+
+            try
+            {
+                e.Node.Nodes.Clear(); // 기존 더미 노드 지움
+                path = e.Node.FullPath;
+                dir = new DirectoryInfo(path);
+                di = dir.GetDirectories();
+
+                foreach(DirectoryInfo dirs in di)
+                {
+                    node = e.Node.Nodes.Add(dirs.Name);
+                    setPlus(node); // 하위에 또 “+” 필요하면 추가
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //해당 폴더의 파일/폴더 목록을 리스트뷰에 표시
+        private void trvDir_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            DirectoryInfo di;
+            DirectoryInfo[] diarray;
+            ListViewItem item;
+            FileInfo[] fiArray;
+
+            try
+            {
+                di = new DirectoryInfo(e.Node.FullPath);
+                txtPath.Text = e.Node.FullPath.Substring(0, 2) + e.Node.FullPath.Substring(3);
+                lvwFiles.Items.Clear();
+
+                diarray = di.GetDirectories();
+                foreach(DirectoryInfo tdls in diarray)
+                {
+                    item = lvwFiles.Items.Add(tdls.Name);
+                    item.SubItems.Add("");
+                    item.SubItems.Add(tdls.LastWriteTime.ToString());
+                    item.ImageIndex = 0;
+                    item.Tag = tdls.FullName;
+                }
+
+                fiArray = di.GetFiles();
+                foreach(FileInfo fls in fiArray)
+                {
+                    item = lvwFiles.Items.Add(fls.Name);
+                    item.SubItems.Add(fls.Length.ToString());
+                    item.SubItems.Add(fls.LastWriteTime.ToString());
+                    item.ImageIndex = 1;
+                    item.Tag = fls.FullName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lvwFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwFiles_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void mnuDetail_Click(object sender, EventArgs e)
+        {
+            mnuDetail.Checked=true;
+            lvwFiles.View = View.Details;
+        }
+
+        private void mnuList_Click(object sender, EventArgs e)
+        {
+            mnuList.Checked=true;
+            lvwFiles.View = View.List;
+        }
+
+        private void mnuSmall_Click(object sender, EventArgs e)
+        {
+            mnuSmall.Checked=true;
+            lvwFiles.View =View.SmallIcon;
+        }
+
+        private void mnuLarge_Click(object sender, EventArgs e)
+        {
+            mnuLarge.Checked=true;
+            lvwFiles.View=View.LargeIcon;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string input = txtSearch.Text.Trim();
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("검색어를 입력하세요.");
+                return;
+            }
+
+            string rootPath;
+            string pattern;
+
+            if (Path.IsPathRooted(input) && Directory.Exists(input))
+            {
+                // 절대경로 검색
+                rootPath = input;
+                pattern = "";
+            }
+            else
+            {
+                // 현재 선택된 경로에서 이름 검색
+                rootPath = txtPath.Text;
+                pattern = input;
+            }
+
+            // * 여기서 txtPath를 갱신 *
+            txtPath.Text = rootPath;
+
+            lvwFiles.Items.Clear();
+            SearchDirectory(rootPath, pattern);
+        }
+
+
+        private void SearchDirectory(string path, string pattern)
+        {
+            try
+            {
+                var dirInfo = new DirectoryInfo(path);
+                // 와일드카드
+                string searchPattern = string.IsNullOrEmpty(pattern)
+                    ? "*"
+                    : "*" + pattern + "*";
+
+                // 1) 바로 이 폴더 안의 매치된 서브폴더
+                foreach (var folder in dirInfo.GetDirectories(searchPattern))
+                {
+                    var item = new ListViewItem(folder.Name);
+                    item.SubItems.Add("");
+                    item.SubItems.Add(folder.LastWriteTime.ToString());
+                    item.ImageIndex = 0;     // 폴더 아이콘
+                    item.Tag = folder.FullName; // 여기에 풀 경로 저장
+                    lvwFiles.Items.Add(item);
+                }
+
+                // 2) 바로 이 폴더 안의 매치된 파일
+                foreach (var file in dirInfo.GetFiles(searchPattern))
+                {
+                    var item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.ImageIndex = 1;     // 파일 아이콘
+                    item.Tag = file.FullName;  //  여기에 풀 경로 저장
+                    lvwFiles.Items.Add(item);
+                }
+
+                // 3) 재귀: 하위 모든 폴더 탐색
+                foreach (var sub in dirInfo.GetDirectories())
+                {
+                    SearchDirectory(sub.FullName, pattern);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // 무시
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("검색 중 오류: " + ex.Message);
+            }
+        }
+
+    }
+}
