df3c5c8ac53fb8c7e40390ff274ed45e3110be8c
Han Seung Jun <97959859+bori99@users.noreply.github.com>
2025-05-09 23:52:38 +0900

    finder_ver02
    sln 인식 테스트

---
+++ Finder_ver02/Finder_ver02/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Finder_ver02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.bnt_search = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmuListView = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDetail = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuList = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSmall = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuLarge = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.trvDir = new System.Windows.Forms.TreeView();
+            this.lvwFiles = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.cmuListView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "전체경로";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(119, 43);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(387, 25);
+            this.txtPath.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(532, 43);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(173, 25);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // bnt_search
+            // 
+            this.bnt_search.Location = new System.Drawing.Point(722, 42);
+            this.bnt_search.Name = "bnt_search";
+            this.bnt_search.Size = new System.Drawing.Size(75, 23);
+            this.bnt_search.TabIndex = 3;
+            this.bnt_search.Text = "검색";
+            this.bnt_search.UseVisualStyleBackColor = true;
+            this.bnt_search.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.bnt_search);
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 79);
+            this.panel1.TabIndex = 4;
+            // 
+            // cmuListView
+            // 
+            this.cmuListView.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuDetail,
+            this.mnuList,
+            this.mnuSmall,
+            this.mnuLarge});
+            this.cmuListView.Name = "cmuListView";
+            this.cmuListView.Size = new System.Drawing.Size(159, 124);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(158, 24);
+            this.mnuOpen.Text = "열기";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuDetail
+            // 
+            this.mnuDetail.Name = "mnuDetail";
+            this.mnuDetail.Size = new System.Drawing.Size(158, 24);
+            this.mnuDetail.Text = "자세히";
+            this.mnuDetail.Click += new System.EventHandler(this.mnuDetail_Click);
+            // 
+            // mnuList
+            // 
+            this.mnuList.Name = "mnuList";
+            this.mnuList.Size = new System.Drawing.Size(158, 24);
+            this.mnuList.Text = "간단히";
+            this.mnuList.Click += new System.EventHandler(this.mnuList_Click);
+            // 
+            // mnuSmall
+            // 
+            this.mnuSmall.Name = "mnuSmall";
+            this.mnuSmall.Size = new System.Drawing.Size(158, 24);
+            this.mnuSmall.Text = "작은 아이콘";
+            this.mnuSmall.Click += new System.EventHandler(this.mnuSmall_Click);
+            // 
+            // mnuLarge
+            // 
+            this.mnuLarge.Name = "mnuLarge";
+            this.mnuLarge.Size = new System.Drawing.Size(158, 24);
+            this.mnuLarge.Text = "큰 아이콘";
+            this.mnuLarge.Click += new System.EventHandler(this.mnuLarge_Click);
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "a.png");
+            this.imgList.Images.SetKeyName(1, "b.png");
+            this.imgList.Images.SetKeyName(2, "c.png");
+            this.imgList.Images.SetKeyName(3, "d.png");
+            // 
+            // trvDir
+            // 
+            this.trvDir.Dock = System.Windows.Forms.DockStyle.Left;
+            this.trvDir.ImageIndex = 0;
+            this.trvDir.ImageList = this.imgList;
+            this.trvDir.Location = new System.Drawing.Point(0, 79);
+            this.trvDir.Name = "trvDir";
+            this.trvDir.SelectedImageIndex = 0;
+            this.trvDir.Size = new System.Drawing.Size(192, 371);
+            this.trvDir.TabIndex = 9;
+            // 
+            // lvwFiles
+            // 
+            this.lvwFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwFiles.HideSelection = false;
+            this.lvwFiles.LargeImageList = this.imgList;
+            this.lvwFiles.Location = new System.Drawing.Point(0, 79);
+            this.lvwFiles.Name = "lvwFiles";
+            this.lvwFiles.Size = new System.Drawing.Size(800, 371);
+            this.lvwFiles.SmallImageList = this.imgList;
+            this.lvwFiles.TabIndex = 8;
+            this.lvwFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trvDir);
+            this.Controls.Add(this.lvwFiles);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.cmuListView.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button bnt_search;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip cmuListView;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuDetail;
+        private System.Windows.Forms.ToolStripMenuItem mnuList;
+        private System.Windows.Forms.ToolStripMenuItem mnuSmall;
+        private System.Windows.Forms.ToolStripMenuItem mnuLarge;
+        private System.Windows.Forms.TreeView trvDir;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.ListView lvwFiles;
+    }
+}
+
