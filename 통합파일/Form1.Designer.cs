16c75777ddc03b4f254e5b95206b43c7bc349dd0
전현규 <yytss3@gmail.com>
2025-05-19 04:02:07 +0900

    통합파일 UI수정

--- 통합파일/Form1.Designer.cs
+++ 통합파일/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -44,22 +45,32 @@
             this.우선순위초기화ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.우선순위정렬ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pathBar = new System.Windows.Forms.Panel();
             this.btn_back = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.trvDir = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.panel1.SuspendLayout();
             this.rightClickMenu.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AllowDrop = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 134);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 23);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1935, 1034);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(932, 518);
             this.flowLayoutPanel1.TabIndex = 2;
             this.flowLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.FlowLayoutPanel1_DragDrop);
             this.flowLayoutPanel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.FlowLayoutPanel1_DragEnter);
@@ -70,37 +81,36 @@
             // 
             // panel1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.SystemColors.Window;
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(0, 88);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1935, 46);
+            this.panel1.Size = new System.Drawing.Size(932, 23);
             this.panel1.TabIndex = 0;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1439, 10);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(775, 5);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 24);
+            this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 5;
             this.label5.Text = "우선순위";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1209, 10);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(651, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 4;
             this.label4.Text = "크기";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -108,10 +118,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(877, 10);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(472, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 3;
             this.label3.Text = "유형";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -119,10 +129,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(574, 10);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(309, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 24);
+            this.label2.Size = new System.Drawing.Size(69, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "수정한 날짜";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -130,10 +140,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 10);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(15, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "이름";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -151,33 +161,33 @@
             this.rightClickMenu.Name = "rightClickMenu";
             this.rightClickMenu.ShowCheckMargin = true;
             this.rightClickMenu.ShowImageMargin = false;
-            this.rightClickMenu.Size = new System.Drawing.Size(265, 206);
+            this.rightClickMenu.Size = new System.Drawing.Size(163, 116);
             // 
             // 삭제ToolStripMenuItem
             // 
             this.삭제ToolStripMenuItem.Name = "삭제ToolStripMenuItem";
-            this.삭제ToolStripMenuItem.Size = new System.Drawing.Size(286, 38);
+            this.삭제ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.삭제ToolStripMenuItem.Text = "삭제";
             this.삭제ToolStripMenuItem.Click += new System.EventHandler(this.삭제ToolStripMenuItem_Click);
             // 
             // 우선순위설정ToolStripMenuItem
             // 
             this.우선순위설정ToolStripMenuItem.Name = "우선순위설정ToolStripMenuItem";
-            this.우선순위설정ToolStripMenuItem.Size = new System.Drawing.Size(286, 38);
+            this.우선순위설정ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.우선순위설정ToolStripMenuItem.Text = "우선순위 설정";
             this.우선순위설정ToolStripMenuItem.Click += new System.EventHandler(this.우선순위설정ToolStripMenuItem_Click);
             // 
             // 우선순위강조ToolStripMenuItem
             // 
             this.우선순위강조ToolStripMenuItem.Name = "우선순위강조ToolStripMenuItem";
-            this.우선순위강조ToolStripMenuItem.Size = new System.Drawing.Size(286, 38);
+            this.우선순위강조ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.우선순위강조ToolStripMenuItem.Text = "우선순위 강조";
             this.우선순위강조ToolStripMenuItem.Click += new System.EventHandler(this.우선순위강조ToolStripMenuItem_Click);
             // 
             // 우선순위초기화ToolStripMenuItem
             // 
             this.우선순위초기화ToolStripMenuItem.Name = "우선순위초기화ToolStripMenuItem";
-            this.우선순위초기화ToolStripMenuItem.Size = new System.Drawing.Size(286, 38);
+            this.우선순위초기화ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.우선순위초기화ToolStripMenuItem.Text = "우선순위 초기화";
             this.우선순위초기화ToolStripMenuItem.Click += new System.EventHandler(this.우선순위초기화ToolStripMenuItem_Click);
             // 
@@ -187,42 +197,62 @@
             this.우선순위정렬ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
             this.우선순위정렬ToolStripMenuItem.Margin = new System.Windows.Forms.Padding(1);
             this.우선순위정렬ToolStripMenuItem.Name = "우선순위정렬ToolStripMenuItem";
-            this.우선순위정렬ToolStripMenuItem.Size = new System.Drawing.Size(264, 40);
+            this.우선순위정렬ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.우선순위정렬ToolStripMenuItem.Text = "우선순위 정렬";
             this.우선순위정렬ToolStripMenuItem.Click += new System.EventHandler(this.우선순위정렬ToolStripMenuItem_Click);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.btnSearch);
+            this.panel2.Controls.Add(this.txtPath);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.pathBar);
             this.panel2.Controls.Add(this.btn_back);
-            this.panel2.Location = new System.Drawing.Point(0, 2);
-            this.panel2.Margin = new System.Windows.Forms.Padding(6);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1935, 84);
+            this.panel2.Size = new System.Drawing.Size(1155, 42);
             this.panel2.TabIndex = 6;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1049, 10);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(66, 22);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(776, 12);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(258, 21);
+            this.txtPath.TabIndex = 5;
             // 
             // button1
             // 
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::priority_file_explorer_.Properties.Resources.addfile;
-            this.button1.Location = new System.Drawing.Point(82, 10);
-            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Location = new System.Drawing.Point(44, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(59, 64);
+            this.button1.Size = new System.Drawing.Size(32, 32);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.파일추가ToolStripMenuItem_Click);
             // 
             // pathBar
             // 
             this.pathBar.BackColor = System.Drawing.SystemColors.Window;
-            this.pathBar.Location = new System.Drawing.Point(377, 10);
-            this.pathBar.Margin = new System.Windows.Forms.Padding(6);
+            this.pathBar.Location = new System.Drawing.Point(203, 5);
             this.pathBar.Name = "pathBar";
-            this.pathBar.Size = new System.Drawing.Size(979, 62);
+            this.pathBar.Size = new System.Drawing.Size(527, 31);
             this.pathBar.TabIndex = 1;
             // 
             // btn_back
@@ -231,22 +261,57 @@
             this.btn_back.FlatAppearance.BorderSize = 0;
             this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_back.Image = global::priority_file_explorer_.Properties.Resources.This_Way;
-            this.btn_back.Location = new System.Drawing.Point(13, 10);
-            this.btn_back.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_back.Location = new System.Drawing.Point(7, 5);
+            this.btn_back.Margin = new System.Windows.Forms.Padding(2);
             this.btn_back.Name = "btn_back";
-            this.btn_back.Size = new System.Drawing.Size(59, 64);
+            this.btn_back.Size = new System.Drawing.Size(32, 32);
             this.btn_back.TabIndex = 0;
             this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 42);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.trvDir);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1155, 541);
+            this.splitContainer1.SplitterDistance = 219;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // trvDir
+            // 
+            this.trvDir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trvDir.ImageIndex = 0;
+            this.trvDir.ImageList = this.imageList1;
+            this.trvDir.Location = new System.Drawing.Point(0, 0);
+            this.trvDir.Name = "trvDir";
+            this.trvDir.SelectedImageIndex = 0;
+            this.trvDir.Size = new System.Drawing.Size(219, 541);
+            this.trvDir.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.png");
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1935, 1166);
+            this.ClientSize = new System.Drawing.Size(1155, 583);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -255,6 +320,11 @@
             this.panel1.PerformLayout();
             this.rightClickMenu.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -278,6 +348,11 @@
         private System.Windows.Forms.Panel pathBar;
         private System.Windows.Forms.Button btn_back;
         private System.Windows.Forms.ToolStripMenuItem 우선순위정렬ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TreeView trvDir;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
